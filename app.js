7a1c0f881b772f30bf0839a26851dad5ef7f1fdc
shuwen <shuwenq@gmail.com>
2014-06-21 00:31:14 -0500

    Some adjustments

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
 	$.each($('li'), function() {
 		$(this).css('position', 'absolute');
-		$(this).css('top', Math.random() * $('body').height());
-		$(this).css('left', Math.random() * $('body').width());
+		$(this).css('top', Math.random() * $('body').height()-100);
+		$(this).css('left', Math.random() * $('body').width()-100);
 		$(this).css('color', colors[ Math.floor(Math.random() * colors.length) ]);
 		$(this).css('font-size', Math.random() * 80);
 	});
